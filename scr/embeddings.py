6d88df41535ded8175f4a746b9e860f24255f124
Yomna Maged <14.yomn@gmail.com>
2025-09-14 15:41:46 +0300

    Refactor embedding process and integrate ChromaDB for knowledge base vector storage

--- scr/embeddings.py
+++ scr/embeddings.py
@@ -1,43 +1,44 @@
-import os
 import json
-import numpy as np
+import chromadb
+from chromadb.config import Settings
+from chromadb.utils import embedding_functions
 from sentence_transformers import SentenceTransformer
-import faiss
 
-model = SentenceTransformer("intfloat/multilingual-e5-base")
+import os
+from dotenv import load_dotenv
+load_dotenv()
+token = os.getenv("HUGGINGFACE_HUB_TOKEN")
+os.environ['HUGGINGFACE_HUB_TOKEN'] = token
+# print(os.getenv("HUGGINGFACE_HUB_TOKEN"))
 
-#Embedding texts in chunks
-def embed_chunks(texts, model):
-    embeddings = model.encode(texts, 
-                             show_progress_bar = True,
-                             normlize_embeddings = True)
-    return embeddings
+# Embedding function
+embedding_fun = embedding_functions.SentenceTransformerEmbeddingFunction(
+    model_name = "intfloat/multilingual-e5-base",
+    normalize_embeddings = True
+)
+
+client = chromadb.PersistentClient(path="./kb_chroma")
 
 #Creating vector database for knowledge base
-def kb_vectors(kb_chunks , model = model):
+def kb_vectors(kb_chunks , collection_name = "kb_vectors",client = client, embedding_fun = embedding_fun):
     with open(kb_chunks, 'r', encoding = 'utf-8') as file:
         chunks = json.load(file)
 
     texts = [chunk['text'] for chunk in chunks]
     ids = [chunk['id'] for chunk in chunks]
+    metadatas = [{"source": chunk.get('source', 'unknoWn')} for chunk in chunks]
 
-    embeddings = embed_chunks(texts, model)
+    if collection_name in [c.name for c in client.list_collections()]:
+        collection = client.get_collection(collection_name,embedding_function = embedding_fun)
+    else:
+        collection = client.create_collection(name = collection_name, embedding_function = embedding_fun) 
+    
+    collection.add(ids=ids, documents = texts, metadatas = metadatas)
 
-    dim = embeddings.shape[1]
-    index = faiss.IndexFlatIP(dim)
-    index.add(embeddings)
-
-    index_file = 'kb_vector.index'
-    meta_file = 'kb_vector.meta.json'
-
-    faiss.write_index(index, index_file)
-
-    with open(meta_file, 'w', encoding = 'utf-8') as file:
-        json.dump(ids, file, ensure_ascii = False, indent = 2)
-
-    return index_file, meta_file
+    return collection_name
 
 
 if __name__ == "__main__":
     kb_chunks = '../kb/kb_chunks.json'
-    index_file, meta_file = kb_vectors(kb_chunks)
+    collection_name = kb_vectors(kb_chunks)
+    print(f"✅ ChromaDB collection '{collection_name}' built and persisted!")
